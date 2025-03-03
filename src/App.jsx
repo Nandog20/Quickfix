507943595b2f46ada641be7ed9eb26feb8aca9c4
Fernando LG <lgfernandomp@gmail.com>
2025-03-03 14:56:10 -0600

    Modificación de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,25 @@
-import './global.css'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
-import Login from './pages/Login'
 
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Products from './pages/Products';
+import Navbar from './components/Navbar';
+import Hero from './pages/Hero';
 
 function App(){
     return(
-        <>
-            <div className="relative w-full h-screen bg-[url('./images/fondo.jpg')] bg-cover bg-center">
-                <div className="absolute inset-0 bg-black/50 backdrop-blur-md flex items-center justify-center">
-                    <h1 className="text-6xl font-bold text-white">Taller QuickFix</h1>
-                </div>
-            </div>
+        <BrowserRouter>
 
-            <div className="max-w-3xl mx-auto px-6 py-10 space-y-6">
-                <h3 className="text-5xl font-bold text-primary border-b-4 pb-2">¿Quiénes somos?</h3>
-                <p className="text-xl text-gray-700 leading-relaxed">
-                QuickFix es un taller mecánico que busca ser uno de los mejores talleres mecánicos de la capital. Estamos ubicados en Avenida San Jerónimo 914, 10200 La Magdalena Contreras, Distrito Federal.
-                Estamos especializados en dar mantenimiento preventivo a automóviles, garantizando que el servicio que damos es de alta calidad.
-                </p>
+      <Navbar/>
 
-                <h3 className="text-5xl font-bold text-primary border-b-4 pb-2">Misión</h3>
-                <p className="text-xl text-gray-700 leading-relaxed">
-                Brindar soluciones integrales y confiables en el mantenimiento y reparación de automóviles, garantizando la satisfacción del cliente mediante un servicio de calidad y atención personalizada.
-                </p>
-
-                <h3 className="text-5xl font-bold text-primary border-b-4 pb-2">Visión</h3>
-                <p className="text-xl text-gray-700 leading-relaxed">
-                Ser reconocidos como el taller mecánico de referencia en la región, destacando por nuestra innovación tecnológica, compromiso con el medio ambiente y excelencia en el servicio.
-                </p>
-
-                <h3 className="text-5xl font-bold text-primary border-b-4 pb-2">Objetivo General</h3>
-                <p className="text-xl text-gray-700 leading-relaxed">
-                Implementar un sistema eficiente de gestión para optimizar la administración de clientes, vehículos y servicios ofreciendo un proceso transparente y confiable que eleve la calidad del taller y la fidelidad del cliente.
-                </p>
-</div>
-
-        </>
+      <Routes>
+        <Route path="/" element={<Hero />}/>
+        <Route path='/register' element={<Register/>} />
+        <Route path='/Login' element={<Login/>}/>
+        <Route path='/Products' element={<Products/>}/>
+      </Routes>
+    </BrowserRouter>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
-
 import App from './App';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Products from './pages/Products';
-import Navbar from './components/Navbar';
-
 
 import './global.css'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-
-      <Navbar/>
-
-      <Routes>
-        <Route path="/" element={<App />}/>
-        <Route path='/register' element={<Register/>} />
-        <Route path='/Login' element={<Login/>}/>
-        <Route path='/Products' element={<Products/>}/>
-      </Routes>
-    </BrowserRouter>
-    
+    <App/>
   </StrictMode>,
 )

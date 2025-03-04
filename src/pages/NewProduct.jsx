f45d917959ad1456039d25b7a89d2bd8fb833937
Fernando LG <lgfernandomp@gmail.com>
2025-03-03 21:44:30 -0600

    Crud para productos

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,27 +1,43 @@
 import Axios from "axios";
-
+import { useState } from "react";
 function NewProduct(){
+    const [producto, setProducto]= useState("");
+    const [stock, setStock]= useState();
+    const [precio, setPrecio]= useState();
+    const [image, setImage]= useState("");
+    const [desc, setDesc]= useState("");
+
+    const Add=()=>{
+        Axios.post("http://localhost:3001/NewProduct",{
+            producto: producto,
+            stock: stock,
+            precio: precio,
+            image: image,
+            desc: desc
+        }).then(()=>alert("Producto agregado"))
+    }
+
     return(
         <div className='py-32 px-4 flex justify-center'>
             <div className='w-full max-w-lg flex flex-col shadow-md p-6 rounded-2xl bg-white'>
                 <h2 className='text-3xl font-bold text-center text-gray-700 m-3'>Ingresa un nuevo producto</h2>
 
                 <label htmlFor="nombre" className='font-semibold text-lg my-2 text-gray-700'>Nombre de producto:</label>
-                <input type="text" name="nombre" placeholder="Producto 1" className='border-slate-200 p-2 rounded-lg bg-slate-100 w-full'/>
+                <input type="text" name="nombre" placeholder="Producto 1" onChange={(e)=>setProducto(e.target.value)} className='border-slate-200 p-2 rounded-lg bg-slate-100 w-full'/>
 
                 <label htmlFor="stock" className='font-semibold text-lg my-2 text-gray-700'>Número de stock:</label>
-                <input type="number" name="stock" placeholder="50" className='border-slate-200 p-2 rounded-lg bg-slate-100 w-full'/>
+                <input type="number" name="stock" placeholder="50" onChange={(e)=>setStock(e.target.value)} className='border-slate-200 p-2 rounded-lg bg-slate-100 w-full'/>
 
-                <label htmlFor="precio" className='font-semibold text-lg my-2 text-gray-700'>Número de stock:</label>
-                <input type="number" name="precio" placeholder="25.50" className='border-slate-200 p-2 rounded-lg bg-slate-100 w-full'/>
+                <label htmlFor="precio" className='font-semibold text-lg my-2 text-gray-700'>Precio producto:</label>
+                <input type="number" name="precio" placeholder="25.50" onChange={(e)=>setPrecio(e.target.value)} className='border-slate-200 p-2 rounded-lg bg-slate-100 w-full'/>
 
                 <label htmlFor="imagen" className='font-semibold text-lg my-2 text-gray-700'>URL imagen:</label>
-                <input type="text" name="imagen" placeholder="../images/imagen.jpg" className='border-slate-200 p-2 rounded-lg bg-slate-100 w-full'/>
+                <input type="text" name="imagen" placeholder="imagen.jpg" onChange={(e)=>setImage(e.target.value)} className='border-slate-200 p-2 rounded-lg bg-slate-100 w-full'/>
 
                 <label htmlFor="descripción" className='font-semibold text-lg my-2 text-gray-700'>Descripción del producto:</label>
-                <input type="text" name="descripción" placeholder="Ingrese la descripción del producto" className='border-slate-200 p-2 rounded-lg bg-slate-100 w-full'/>
+                <input type="text" name="descripción" placeholder="Ingrese la descripción del producto" onChange={(e)=>setDesc(e.target.value)} className='border-slate-200 p-2 rounded-lg bg-slate-100 w-full'/>
 
-                <button className=' my-4 border bg-cyan-500 text-white w-full rounded-lg py-2 shadow-lg hover:bg-cyan-600 transition cursor-pointer'>Añadir producto</button>
+                <button onClick={Add} className=' my-4 border bg-cyan-500 text-white w-full rounded-lg py-2 shadow-lg hover:bg-cyan-600 transition cursor-pointer' >Añadir producto</button>
             </div>
         </div>
     )
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,37 @@
 import Axios from "axios"
+import { useState, useEffect } from "react"
+
 function Products(){
-    
+
+    const [productos, setProductos] = useState([])
+
+    useEffect(() => {
+        Axios.get("http://localhost:3001/products")
+            .then((response) => {
+                setProductos(response.data);
+            }).catch((error) => console.error("Error al obtener productos:", error));
+    }, []);
+
+    return(
+        <div className="pt-20">
+            <div className="grid grid-cols-3 gap-4 p-4">
+            {productos.map((producto) => (
+                <div id={producto.id} className="border p-4 rounded-lg shadow-md">
+                    <img 
+                        src={`http://localhost:5173/src/images/${producto.img}`} 
+                        alt={producto.nombre} 
+                        className="w-full h-[400px] object-cover mb-2 rounded-lg" 
+                    />
+                    <h2 className="text-lg font-semibold">{producto.nombre}</h2>
+                    <p className="text-gray-600">${producto.precio}</p>
+                    <p className="text-gray-600">piezas: {producto.stock}</p>
+                    <p className="text-gray-600">{producto.descripción}</p>
+                    <button>Comprar</button>
+                </div>
+            ))}
+        </div>
+        </div>
+    )
 }
 
 export default Products
